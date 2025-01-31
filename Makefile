e30c452466fdf9c8456c06b126bb35dc140ba945
MrShanks <simonestaffoli@gmail.com>
2025-01-31 22:33:51 +0100

    feat: TAS-10 add faulty test

--- Makefile
+++ Makefile
@@ -4,7 +4,7 @@ build:
 	go build -v -ldflags="-X github.com/MrShanks/Taska/cmd.version=$(shell yq '.app.version' config.yaml)" -o taskcli .
 
 test:
-	go test .
+	go test -v ./...
 
 run:
-	go run .
+	go run ./...
---
+++ cmd/root_test.go
@@ -0,0 +1,30 @@
+package cmd
+
+import (
+	"bytes"
+	"testing"
+)
+
+func TestRootCmd(t *testing.T) {
+	t.Run("Run rootCmd should return default message", func(t *testing.T) {
+		var capture bytes.Buffer
+		rootCmd.SetOut(&capture)
+
+		err := rootCmd.Execute()
+		if err != nil {
+			t.Fatalf("rootCmd.Execute() error = %v, want nil", err)
+		}
+
+		// expected := `taskcli is your CLI best friend`
+		expected := `test build should fail and merge should be blocked`
+		output := capture.String()
+
+		if !contains(output, expected) {
+			t.Errorf("Expected output to contain %q, but got %q", expected, output)
+		}
+	})
+}
+
+func contains(output, expected string) bool {
+	return bytes.Contains([]byte(output), []byte(expected))
+}
