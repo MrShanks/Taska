4b4c31fe5824997c85402777148138bdd442d1dd
MrShanks <simonestaffoli@gmail.com>
2025-02-03 14:19:47 +0100

    fix: TAS-20 resolve issue with dir and bin that have same name

---
+++ taskcli/cmd/version.go
@@ -0,0 +1,27 @@
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/MrShanks/Taska/utils"
+	"github.com/spf13/cobra"
+)
+
+var version = "undefined"
+
+var versionCmd = &cobra.Command{
+	Use:   "version",
+	Short: "Print taskcli version",
+	Long:  `This command reads the version from ~/.Taska.yaml config file`,
+
+	Run: func(cmd *cobra.Command, args []string) {
+		if version == "undefined" {
+			version = utils.ReadVersionFromConfig()
+		}
+		fmt.Printf("version: %s\n", version)
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(versionCmd)
+}
