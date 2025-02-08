013a8e53de9ade4c24d8252dead766be5a162878
MrShanks <simonestaffoli@gmail.com>
2025-02-08 18:50:01 +0100

    feat: TAS-14 add get command to list all tasks
    get command lists all tasks available on the server
    it initialize a http client and issues get requests
    on the /tasks endpoint. When the next commands will
    be added we would need to mover the initialization
    of the api client someplace else.

--- taskcli/cmd/root.go
+++ taskcli/cmd/root.go
@@ -1,11 +1,9 @@
 package cmd
 
 import (
-	"fmt"
 	"os"
 
 	"github.com/spf13/cobra"
-	"github.com/spf13/viper"
 )
 
 var cfgFile string
@@ -40,8 +38,6 @@ func Execute() {
 }
 
 func init() {
-	cobra.OnInitialize(initConfig)
-
 	// Here you will define your flags and configuration settings.
 	// Cobra supports persistent flags, which, if defined here,
 	// will be global for your application.
@@ -51,29 +47,4 @@ func init() {
 	// Cobra also supports local flags, which will only run
 	// when this action is called directly.
 	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
-}
-
-// initConfig reads in config file and ENV variables if set.
-func initConfig() {
-	if cfgFile != "" {
-		// Use config file from the flag.
-		viper.SetConfigFile(cfgFile)
-	} else {
-		// Find home directory.
-		home, err := os.UserHomeDir()
-		cobra.CheckErr(err)
-
-		// Search config in home directory with name ".Taska" (without extension).
-		viper.AddConfigPath(home)
-		viper.SetConfigType("yaml")
-		viper.SetConfigName(".Taska")
-	}
-
-	viper.AutomaticEnv() // read in environment variables that match
-
-	// If a config file is found, read it in.
-	if err := viper.ReadInConfig(); err != nil {
-		fmt.Fprintln(os.Stderr, "couldn't find config file in ", viper.ConfigFileUsed())
-		os.Exit(1)
-	}
 }
