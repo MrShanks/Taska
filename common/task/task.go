997d9163b11e05ed2a4fe2bb5d64779c1339d98b
Simone <simonestaffoli@gmail.com>
2025-02-12 00:03:28 +0100

    feat: TAS-9 add /tasks endpoint to return a list of tasks (#9)
    * add get tasks endpoint
    
    * add new task endpoint

---
+++ common/task/task.go
@@ -0,0 +1,24 @@
+package task
+
+import (
+	"github.com/google/uuid"
+)
+
+type Task struct {
+	ID    uuid.UUID `json:"id"`
+	Title string    `json:"title"`
+	Desc  string    `json:"desc"`
+}
+
+func New(title, desc string) *Task {
+	return &Task{
+		ID:    uuid.New(),
+		Title: title,
+		Desc:  desc,
+	}
+}
+
+type TaskStore interface {
+	GetTasks() []*Task
+	New(*Task)
+}
--- taskmgr/server/server.go
+++ taskmgr/server/server.go
@@ -1,22 +1,121 @@
 package server
 
 import (
+	"encoding/json"
 	"errors"
+	"fmt"
 	"io"
 	"net/http"
 	"os"
 
-	"github.com/MrShanks/Taska/taskmgr/logger"
+	"github.com/google/uuid"
+
+	"github.com/MrShanks/Taska/common/logger"
+	"github.com/MrShanks/Taska/common/task"
 )
 
-func HomeHandler(w http.ResponseWriter, r *http.Request) {
-	logger.InfoLogger.Println("Got request on / endpoint")
-	io.WriteString(w, "Welcome to your dashboard\n")
+// inMemoryDatabase implements the taskStore interface
+type inMemoryDatabase struct {
+	tasks []*task.Task
 }
 
+func (md *inMemoryDatabase) GetTasks() []*task.Task {
+	return md.tasks
+}
+
+func (md *inMemoryDatabase) New(task *task.Task) {
+	md.tasks = append(md.tasks, task)
+}
+
+func GetHandler(store task.TaskStore) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != http.MethodGet {
+			logger.ErrorLogger.Printf("Method: %s is not allowed on /tasks endpoint\n", r.Method)
+
+			w.WriteHeader(http.StatusMethodNotAllowed)
+			return
+		}
+
+		logger.InfoLogger.Printf("Got request on /tasks endpoint\n")
+
+		jsonTasks, err := json.Marshal(store.GetTasks())
+		if err != nil {
+			logger.ErrorLogger.Printf("Couldn't Marshal tasks into json format: %v", err)
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+
+		w.Header().Set("Content-Type", "application/json")
+		_, err = w.Write(jsonTasks)
+		if err != nil {
+			logger.ErrorLogger.Printf("Couldn't write response: %v", err)
+		}
+	}
+}
+
+func NewTaskHandler(store task.TaskStore) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != http.MethodPost {
+			logger.ErrorLogger.Printf("Method: %s is not allowed on /new endpoint\n", r.Method)
+			w.WriteHeader(http.StatusMethodNotAllowed)
+			return
+		}
+
+		logger.InfoLogger.Printf("Got request on /new endpoint\n")
+
+		body, err := io.ReadAll(r.Body)
+		if err != nil {
+			logger.ErrorLogger.Printf("Couldn't read the body. Error type: %s", err)
+		}
+		defer r.Body.Close()
+
+		newTask := task.Task{
+			ID: uuid.New(),
+		}
+
+		err = json.Unmarshal(body, &newTask)
+		if err != nil {
+			http.Error(w, "Invalid JSON format", http.StatusBadRequest)
+			return
+		}
+		store.New(&newTask)
+
+		logger.InfoLogger.Printf("New task created.\nID: %s", newTask.ID)
+		w.WriteHeader(http.StatusCreated)
+		w.Write([]byte(fmt.Sprintf("New task created.\nID: %s\nTitle: %s\nDesc: %s", newTask.ID, newTask.Title, newTask.Desc)))
+	}
+}
+
+func homeHandler(w http.ResponseWriter, r *http.Request) {
+	logger.InfoLogger.Println("Got request on / endpoint")
+	_, err := io.WriteString(w, "Welcome to your dashboard\n")
+	if err != nil {
+		logger.ErrorLogger.Printf("Couldn't write response: %v", err)
+	}
+}
+
+func faviconHandler(w http.ResponseWriter, r *http.Request) {
+	// Just return a blank 204 response (No Content)
+	// This is needed because browser always issue a second
+	// http request to get the favicon for a website
+	w.WriteHeader(http.StatusNoContent)
+}
+
+// Listen initialize the server and waits for requests
 func Listen(version string) {
+	var IMD = inMemoryDatabase{
+		tasks: []*task.Task{
+			task.New("first", "Desc First"),
+			task.New("second", "Desc Second"),
+			task.New("third", "Desc Third"),
+		},
+	}
+
 	webMux := http.NewServeMux()
-	webMux.HandleFunc("/", HomeHandler)
+	webMux.HandleFunc("/", homeHandler)
+	webMux.HandleFunc("/tasks", GetHandler(&IMD))
+	webMux.HandleFunc("/new", NewTaskHandler(&IMD))
+	webMux.HandleFunc("/favicon.ico", faviconHandler)
 
 	httpServer := &http.Server{
 		Addr:    ":8080",
