3ca65493917a138ef3e297e57157b330c3981f46
Andrea <andreapinchera.1989@gmail.com>
2025-02-13 18:10:35 +0100

    feat: TAS-22 add test on newTaskHandler

--- taskmgr/server/server_test.go
+++ taskmgr/server/server_test.go
@@ -1,7 +1,10 @@
 package server
 
 import (
+	"bytes"
 	"encoding/json"
+	"github.com/google/uuid"
+	"io"
 	"net/http"
 	"net/http/httptest"
 	"testing"
@@ -38,4 +41,59 @@ func TestGetTasksHandler(t *testing.T) {
 		}
 	})
 
+}
+
+func TestNewTaskHandler(t *testing.T) {
+
+	var IMD = inMemoryDatabase{
+		tasks: []*task.Task{},
+	}
+
+	newDummyTask := struct {
+		Title string
+		Desc  string
+	}{
+		Title: "new upcomig task",
+		Desc:  "title of a new fancy task",
+	}
+
+	t.Run("check new task created", func(t *testing.T) {
+
+		jsonTask, err := json.Marshal(newDummyTask)
+		if err != nil {
+			t.Errorf("Unable to marshal the dummy task")
+			return
+		}
+
+		request, _ := http.NewRequest(http.MethodPost, "/new", bytes.NewBuffer(jsonTask))
+
+		body, err := io.ReadAll(request.Body)
+		if err != nil {
+			t.Errorf("Couldn't read the body. Error type: %s", err)
+		}
+		defer request.Body.Close()
+
+		newTask := task.Task{
+			ID: uuid.New(),
+		}
+
+		err = json.Unmarshal(body, &newTask)
+		if err != nil {
+			t.Errorf("Invalid JSON format")
+			return
+		}
+		IMD.New(&newTask)
+
+		if len(IMD.tasks) == 0 {
+			t.Errorf("Expected a task to be added, but the task list is empty")
+		} else {
+			got := IMD.tasks[0].Title
+			want := newDummyTask.Title
+
+			if got != want {
+				t.Errorf("got %q, want %q", got, want)
+			}
+		}
+
+	})
 }
