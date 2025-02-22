129454a30ea967ecdf3506b3502e72e7564a6ec5
Andrea <andreapinchera.1989@gmail.com>
2025-02-22 12:26:30 +0100

    feat/TAS-22 address notes from mastro review

--- taskmgr/server/server_test.go
+++ taskmgr/server/server_test.go
@@ -4,7 +4,6 @@ import (
 	"bytes"
 	"context"
 	"encoding/json"
-	"fmt"
 	"net/http"
 	"net/http/httptest"
 	"testing"
@@ -46,67 +45,14 @@ func TestNewTaskHandler(t *testing.T) {
 		want := task.New(newDummyTask.Title, newDummyTask.Desc)
 		got := mockDatabase.GetTasks()
 		if len(got) == 0 {
-			t.Errorf("unable to fetch the tasks, task didn't created")
+			t.Errorf("Expected got != %v", len(got))
+			return
 		}
-		fmt.Printf("GOT  	%v\n", got[0].Title)
-		fmt.Printf("WANT 	%v\n", want.Title)
+		t.Logf("GOT  	%v\n", got[0].Title)
+		t.Logf("WANT 	%v\n", want.Title)
 		if got[0].Title != want.Title {
 			t.Errorf("got: %v, want: %v", got[0].Title, want.Title)
+			return
 		}
 	})
 }
-
-// func TestNewTaskHandler(t *testing.T) {
-
-// 	var IMD = inMemoryDatabase{
-// 		tasks: []*task.Task{},
-// 	}
-
-// 	newDummyTask := struct {
-// 		Title string
-// 		Desc  string
-// 	}{
-// 		Title: "new upcomig task",
-// 		Desc:  "title of a new fancy task",
-// 	}
-
-// 	t.Run("check new task created", func(t *testing.T) {
-
-// 		jsonTask, err := json.Marshal(newDummyTask)
-// 		if err != nil {
-// 			t.Errorf("Unable to marshal the dummy task")
-// 			return
-// 		}
-
-// 		request, _ := http.NewRequest(http.MethodPost, "/new", bytes.NewBuffer(jsonTask))
-
-// 		body, err := io.ReadAll(request.Body)
-// 		if err != nil {
-// 			t.Errorf("Couldn't read the body. Error type: %s", err)
-// 		}
-// 		defer request.Body.Close()
-
-// 		newTask := task.Task{
-// 			ID: uuid.New(),
-// 		}
-
-// 		err = json.Unmarshal(body, &newTask)
-// 		if err != nil {
-// 			t.Errorf("Invalid JSON format")
-// 			return
-// 		}
-// 		IMD.New(&newTask)
-
-// 		if len(IMD.tasks) == 0 {
-// 			t.Errorf("Expected a task to be added, but the task list is empty")
-// 		} else {
-// 			got := IMD.tasks[0].Title
-// 			want := newDummyTask.Title
-
-// 			if got != want {
-// 				t.Errorf("got %q, want %q", got, want)
-// 			}
-// 		}
-
-// 	})
-// }
