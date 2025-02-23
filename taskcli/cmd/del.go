77886eef9b8bb8354d920ffdd711903261ef211b
MrShanks <simonestaffoli@gmail.com>
2025-02-23 18:25:27 +0100

    feat: TAS-15 add delete cmd and test

---
+++ taskcli/cmd/del.go
@@ -0,0 +1,59 @@
+package cmd
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/spf13/cobra"
+)
+
+var delCmd = &cobra.Command{
+	Use:   "del",
+	Short: "Delete a task",
+	Long:  "Delete a task by passing its id",
+
+	Run: func(cmd *cobra.Command, args []string) {
+		ctx := context.Background()
+		apiClient := NewApiClient()
+
+		id, err := cmd.Flags().GetString("id")
+		if err != nil {
+			cmd.Printf("Couldn't retrieve id flag: %v", err)
+		}
+
+		cmd.Printf("%s", delTask(apiClient, ctx, fmt.Sprintf("/delete/%s", id)))
+	},
+}
+
+func delTask(taskcli *Taskcli, ctx context.Context, endpoint string) string {
+	taskcli.ServerURL.Path = endpoint
+
+	request, err := http.NewRequestWithContext(ctx, "DELETE", taskcli.ServerURL.String(), nil)
+	if err != nil {
+		return fmt.Sprintf("Couldn't create request: %v", err)
+	}
+
+	response, err := taskcli.HttpClient.Do(request)
+	if err != nil {
+		return fmt.Sprintf("Couldn't get a response from the server: %v", err)
+	}
+
+	if response.StatusCode == http.StatusNotFound {
+		return "Task not found"
+	}
+
+	return "Task Successfully deleted"
+}
+
+func init() {
+	delCmd.PersistentFlags().StringP("id", "i", "xxxxx000-0x00-0xx0-x0x0-000x00xx00x0", "Id of the task you want to delete")
+
+	err := delCmd.MarkPersistentFlagRequired("id")
+	if err != nil {
+		log.Printf("Error marking persisten flag required: %v", err)
+	}
+
+	rootCmd.AddCommand(delCmd)
+}
--- taskcli/cmd/get.go
+++ taskcli/cmd/get.go
@@ -4,7 +4,6 @@ import (
 	"context"
 	"fmt"
 	"io"
-	"log"
 	"net/http"
 
 	"github.com/spf13/cobra"
@@ -12,8 +11,8 @@ import (
 
 var getCmd = &cobra.Command{
 	Use:   "get",
-	Short: "get all active tasks",
-	Long:  "get all active tasks store on the server",
+	Short: "Get all active tasks",
+	Long:  "Get all active tasks store on the server",
 
 	Run: func(cmd *cobra.Command, args []string) {
 		ctx := context.Background()
@@ -28,19 +27,19 @@ func FetchTasks(taskcli *Taskcli, ctx context.Context, endpoint string) string {
 
 	request, err := http.NewRequestWithContext(ctx, "GET", taskcli.ServerURL.String(), nil)
 	if err != nil {
-		log.Printf("Couldn't create request: %v", err)
+		return fmt.Sprintf("Couldn't create request: %v", err)
 	}
 
 	response, err := taskcli.HttpClient.Do(request)
 	if err != nil {
-		log.Printf("Couldn't get a response from the server: %v", err)
+		return fmt.Sprintf("Couldn't get a response from the server: %v", err)
 	}
 
 	bodyBytes, err := io.ReadAll(response.Body)
-	if err != nil {
-		log.Printf("Couldn't read response body: %v", err)
-	}
 	defer response.Body.Close()
+	if err != nil {
+		return fmt.Sprintf("Couldn't read response body: %v", err)
+	}
 
 	return fmt.Sprintf("%v", string(bodyBytes))
 }
