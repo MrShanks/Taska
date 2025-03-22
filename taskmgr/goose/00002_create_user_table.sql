fc8dc031d0a564d8c887027cfec2b3ba9e944fe3
MrShanks <simonestaffoli@gmail.com>
2025-03-22 15:23:00 +0100

    feat: remove fake author

--- taskmgr/goose/00002_create_user_table.sql
+++ taskmgr/goose/00002_create_user_table.sql
@@ -8,17 +8,14 @@ CREATE TABLE author (
     password TEXT
 );
 
-TRUNCATE TABLE task;
-
 ALTER TABLE task ADD COLUMN author_id UUID NOT NULL DEFAULT gen_random_uuid();
 ALTER TABLE task ADD CONSTRAINT fk_author FOREIGN KEY (author_id) REFERENCES author(id) ON DELETE CASCADE;
-
-INSERT INTO author (firstname, lastname, email, password) VALUES('marco', 'rossi', 'marco@rossi.com', 'password')
 -- +goose StatementEnd
 
 -- +goose Down
 -- +goose StatementBegin
 ALTER TABLE task DROP CONSTRAINT fk_author;
 ALTER TABLE task DROP COLUMN author_id;
+
 DROP TABLE author;
 -- +goose StatementEnd
