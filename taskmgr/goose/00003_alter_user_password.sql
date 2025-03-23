9393f63dc8526fd1bc57f9eb379f1f6fe0697869
Andrea <andreapinchera.1989@gmail.com>
2025-03-23 09:45:17 +0100

    update password type in postgres

--- taskmgr/goose/00003_alter_user_password.sql
+++ taskmgr/goose/00003_alter_user_password.sql
@@ -1,11 +1,9 @@
 -- +goose Up
 -- +goose StatementBegin
-ALTER TABLE author ALTER COLUMN password TYPE char(60);
 ALTER TABLE author ALTER COLUMN password SET NOT NULL;
 -- +goose StatementEnd
 
 -- +goose Down
 -- +goose StatementBegin
 ALTER TABLE author ALTER COLUMN password DROP NOT NULL;
-ALTER TABLE author ALTER COLUMN password TYPE TEXT;
 -- +goose StatementEnd
