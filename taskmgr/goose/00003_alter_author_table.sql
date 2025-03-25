f7dad451a1263a12312d5212ccba20b995c3c8a1
Andrea <andreapinchera.1989@gmail.com>
2025-03-25 10:54:01 +0100

    remove duplicated 00003 goose version

--- taskmgr/goose/00003_alter_author_table.sql
+++ taskmgr/goose/00003_alter_author_table.sql
@@ -1,9 +1,15 @@
 -- +goose Up
 -- +goose StatementBegin
+
 ALTER TABLE author ALTER COLUMN password SET NOT NULL;
+ALTER TABLE author ADD COLUMN token TEXT;
+
 -- +goose StatementEnd
 
 -- +goose Down
 -- +goose StatementBegin
+
+ALTER TABLE author DROP COLUMN token;
 ALTER TABLE author ALTER COLUMN password DROP NOT NULL;
+
 -- +goose StatementEnd
